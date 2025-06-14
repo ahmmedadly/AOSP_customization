5ceec6565f2b86f1e7873efabca9919507f820f4
ahmmedadly <ahmeddly30971@gmail.com>
2025-06-14 08:04:59 +0300

    Updated lab 9 with I2C

--- lab9/VhalApplication/app/src/main/java/com/vhal/test/MainActivity.kt
+++ lab9/VhalApplication/app/src/main/java/com/vhal/test/MainActivity.kt
@@ -1,29 +1,38 @@
 package com.vhal.test
 
-import android.app.Application
 import android.car.Car
-import android.car.hardware.CarPropertyValue
 import android.car.hardware.property.CarPropertyManager
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import java.lang.Integer
+import android.app.Application
+import android.car.hardware.CarPropertyValue
+import androidx.activity.enableEdgeToEdge
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
-    val  VENDOR_EXTENSION_LED_CONTROL_PROPERTY:Int = 0x21400104
-    val areaID = 0
-     lateinit var car:Car
-     lateinit var carPropertyManager:CarPropertyManager
+    private val VENDOR_EXTENSION_LED_CONTROL_PROPERTY = 0x21400104
+    private val VENDOR_EXTENSION_I2C_CONTROL_INT_PROPERTY = 0x21400105
+    private val areaID = 0
+
+    private lateinit var car: Car
+    private lateinit var carPropertyManager: CarPropertyManager
+
+    private lateinit var seekBar: SeekBar
+    private lateinit var resultTextView: TextView
+    private var isRunning = true
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         val btnOn = findViewById<Button>(R.id.btnOn)
         val btnOff = findViewById<Button>(R.id.btnOff)
-
+        seekBar = findViewById<SeekBar>(R.id.seekBar)
+        resultTextView=  findViewById(R.id.resultTextView)
         btnOff.setOnClickListener {
             setLedState(Integer(0))
         }
@@ -37,7 +46,10 @@ class MainActivity : AppCompatActivity() {
             carPropertyManager = car!!.getCarManager(Car.PROPERTY_SERVICE) as CarPropertyManager
             Log.e("Steering", "CarPropertyManager initialized successfully")
         }
+        startI2cValueUpdater()
+
     }
+
     fun setLedState(ledState:Integer) {
         try {
             synchronized(carPropertyManager) {
@@ -49,5 +61,33 @@ class MainActivity : AppCompatActivity() {
         } catch (e: Exception) {
             Log.e("LedState", "Error set Led State", e)
         }
+    }
+
+    private fun startI2cValueUpdater() {
+        Thread {
+            while (isRunning) {
+                try {
+                    val propertyValue: CarPropertyValue<Integer>? = carPropertyManager.getProperty(
+                        Integer::class.java, VENDOR_EXTENSION_I2C_CONTROL_INT_PROPERTY, 0
+                    )
+                    val value = propertyValue?.getValue()?.toInt() ?: -1
+                    val mapped = (value * 100) / 66
+
+                    runOnUiThread {
+                        seekBar.setProgress(mapped)
+                        resultTextView.text = "Mapped Value: $mapped"
+                    }
+                } catch (e: Exception) {
+                    Log.e("I2C", "Error reading I2C property", e)
+                }
+
+                Thread.sleep(500)
+            }
+        }.start()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        isRunning = false
     }
 }
